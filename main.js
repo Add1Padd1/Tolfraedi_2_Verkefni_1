e31605eed6a220f6d0ba7f4c683cc8101fc32a3b
Add1Padd1 <arnaldur100@gmail.com>
2025-02-04 20:07:35 +0000

    færa skrif á index.html

--- main.js
+++ main.js
@@ -1,24 +1,79 @@
 import fs from 'node:fs/promises';
-import path from 'node:path';
+import path, { parse } from 'node:path';
 const INDEX_PATH = './data/index.json';
-
+/**
+ * Les skrá og skilar gögnum eða null.
+ * @param {string} filePath Skráin sem á að lesa
+ * @returns {Promise<unknown | null>} Les skrá úr `filepath` og skilar innihaldi. Skilar `null` ef villa kom upp
+ */
 async function readJson(filePath) {
   console.log('starting to read', filePath);
+  let data;
   try {
-    const data = await fs.readFile(path.resolve(filePath), 'utf-8');
-    return JSON.parse(data);
+    data = await fs.readFile(path.resolve(filePath), 'utf-8');
   } catch (error) {
     console.error(`Error reading file ${filePath}:`, error.message);
     return null;
   }
+
+  try {
+    const parsed = JSON.parse(data);
+    return parsed;
+  } catch (error) {
+    console.error('Error parsing data as JSON');
+    return null;
+  }
+}
+/**
+ * Skrifa HTML fyrir yfirlit í index.html
+ * @param {any} data Gögn til að skrifa
+ * @returns {Promise<void>}
+ */
+async function writeHtml(data) {
+  const htmlFilePath = 'dist/index.html';
+
+  let html = '';
+  for (const item of data) {
+    html += `<li>${item.title}</li>\n`;
+  }
+  const htmlContent = `
+  <!doctype html>
+  <html>
+    <head>
+    <title>v1</title>
+    </head>
+    <body>
+      <ul>
+        ${html}
+      </ul>
+    </body>
+  </html>
+  `;
+  fs.writeFile(htmlFilePath, htmlContent, 'utf8');
 }
 
+/**
+ *
+ * @param {unknown} data
+ * @returns {any}
+ */
+function parseIndexJson(data) {
+  return data;
+}
+/**
+ * Keyrir forritið okkar:
+ * 1. Sækir gögn
+ * 2. Staðfestir gögn (validation)
+ * 3. Skrifar út HTML
+ */
 async function main() {
-  console.log('Hello world');
+  const indexJson = await readJson(INDEX_PATH);
 
-  const data = await readJson(INDEX_PATH);
+  const indexData = parseIndexJson(indexJson);
 
-  console.log(data);
+  writeHtml(indexData);
+
+  console.log(indexData);
 }
 
 main();
